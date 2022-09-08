08c54905b4458abda52b890752540d76ce3c2780
Manthansinh099 <88449523+Manthansinh099@users.noreply.github.com>
2022-09-08 21:45:40 +0530

    Added Security with custom Users jdbc authentication using H2 database.

--- src/main/java/com/example/studentbckenddemo/demo/Config/securityconfig.java
+++ src/main/java/com/example/studentbckenddemo/demo/Config/securityconfig.java
@@ -1,2 +1,50 @@
-package com.example.studentbckenddemo.demo.Config;public class securityconfig {
+package com.example.studentbckenddemo.demo.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class securityconfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("admin")
+//                .password("admin1")
+//                .roles("ADMIN", "USER")
+//                .and()
+//                .withUser("user")
+//                .password("pwd")
+//                .roles("USER");
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser("admin")
+                .password("admin1")
+                .roles("ADMIN", "USER")
+                .and()
+                .withUser("user")
+                .password("pwd")
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
+//    public PasswordEncoder getPasswordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+
+
